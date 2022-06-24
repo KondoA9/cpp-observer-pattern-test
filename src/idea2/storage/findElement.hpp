262b4ef5f641cb806598e641c3d19c18799260e4
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-06-24 19:38:51 +0900

    refactor

--- src/idea2/storage/findElement.hpp
+++ src/idea2/storage/findElement.hpp
@@ -16,14 +16,15 @@ std::optional<size_t> findElement(const std::vector<T>& vector,
     size_t left = 0, right = vector.size() - 1;
 
     while (right >= left) {
-        const size_t mid = left + (right - left) / 2;
+        const size_t mid = (left + right) / 2;
+        const auto value = getValue(vector[mid]);
 
-        if (const auto value = getValue(vector[mid]); value == key) {
-            return mid;
+        if (value < key) {
+            left = mid + 1;
         } else if (value > key) {
             right = mid - 1;
         } else {
-            left = mid + 1;
+            return mid;
         }
     }
 

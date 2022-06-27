a359e22c26eccd860ffc4f3dcb6ade15bcd02e74
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-06-27 11:56:29 +0900

    update

--- src/idea2/state/state.hpp
+++ src/idea2/state/state.hpp
@@ -12,7 +12,9 @@ namespace Internal {
 
 template <typename T>
 class State final : public Internal::IState {
-    using OnChangeFuncType = std::function<void(const T& current, const T& previous)>;
+    using OnChangeFuncType  = std::function<void(const T& current, const T& previous)>;
+    using OnChangeFuncType2 = std::function<void(const T& current)>;
+    using OnChangeFuncType3 = std::function<void()>;
 
     friend class Internal::StateGroup<T>;
     friend class StateFactory;
@@ -28,13 +30,23 @@ public:
     void bind(const State<T>& state) {
         // Bind if not in the same group
         if (_stateGroupId() != state._stateGroupId()) {
-            auto& group = state.getGroup();
+            const auto prevValue = value();
+            auto& group          = state.getGroup();
             group.addState(_stateId());
+            fireOnChange(value(), prevValue);
         }
     }
 
-    void setOnChange(const OnChangeFuncType& func) {
-        m_onChange = func;
+    void setOnChange(const OnChangeFuncType& func, bool inherit = false) {
+        setOnChangeImpl(func, inherit);
+    }
+
+    void setOnChange(const OnChangeFuncType2& func, bool inherit = false) {
+        setOnChangeImpl(std::bind(func, std::placeholders::_1), inherit);
+    }
+
+    void setOnChange(const OnChangeFuncType3& func, bool inherit = false) {
+        setOnChangeImpl(std::bind(func), inherit);
     }
 
     void setValue(const T& newValue) {
@@ -73,6 +85,19 @@ private:
     void fireOnChange(const T& current, const T& previous) const {
         if (m_onChange) {
             m_onChange(current, previous);
+        }
+    }
+
+    void setOnChangeImpl(const OnChangeFuncType& func, bool inherit) {
+        if (inherit && m_onChange) {
+            const auto prevOnChange = m_onChange;
+
+            m_onChange = [prevOnChange, func](const T& current, const T& previous) {
+                prevOnChange(current, previous);
+                func(current, previous);
+            };
+        } else {
+            m_onChange = func;
         }
     }
 };
